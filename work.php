d53dc82a72046ee435f5b2b22a43e60052c6082c
ali_hassam <alykhanhssm@gmail.com>
2019-02-12 14:35:19 -0700

    Update work.php

--- work.php
+++ work.php
@@ -3,7 +3,20 @@
 <body>
 
 <?php
-echo "Yes, it worked!";
+//convert the JSON file into a string
+$string = file_get_contents("CalgarySchools.geojson");
+
+//decode the String into a PHP object
+$obj = json_decode($string, true);
+echo "<div id='jsonContent'>";
+echo "<pre>";
+print_r ($obj);
+
+$stringconvertedback = json_encode($obj, true);
+
+echo $stringconvertedback; 
+echo "</div>";
+
 
 ?> 
 
