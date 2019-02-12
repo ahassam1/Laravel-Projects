3b60dd1d4808189a2b15ffbd68ffc7660fb693ba
ali_hassam <alykhanhssm@gmail.com>
2019-02-12 14:37:52 -0700

    fix

--- process.php
+++ process.php
@@ -3,7 +3,22 @@
 <body>
 
 <?php
-include 'work.php';
+
+//convert the JSON file into a string
+$string = file_get_contents("CalgarySchools.geojson");
+
+//decode the String into a PHP object
+$obj = json_decode($string, true);
+echo "<div id='jsonContent'>";
+echo "<pre>";
+print_r ($obj);
+
+$stringconvertedback = json_encode($obj, true);
+
+echo $stringconvertedback; 
+echo "</div>";
+
+
 ?> 
 
 </body>
