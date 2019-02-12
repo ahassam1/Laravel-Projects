482c1550b50582d7529fb037ab2c423d84ef9f82
ali_hassam <alykhanhssm@gmail.com>
2019-02-12 15:52:33 -0700

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -7,7 +7,30 @@ Point 1 Latitude: <?php echo $_GET["p1x"]; ?><br>
 Point 1 Longitude: <?php echo $_GET["p1y"]; ?> <br>
 Point 2 Latitude: <?php echo $_GET["p2x"]; ?><br>
 Point 2 Longitude: <?php echo $_GET["p2y"]; ?>
+<br><br>
+<br>
+<br>
+<br>
+<br>
 
+<?php
+
+$p1y = $_GET["p1x"];
+$p2y = $_GET["p2x"];
+$p1x = $_GET["p1y"];
+$p2x = $_GET["p2y"];
+
+if(empty($p1x) || empty($p2x) || empty($p2x) || empty($p2y))
+{
+	echo "Empty value detected";
+}
+
+if($p1y < -90 || $p1y > 90 || $p2y < -90 || $p2y > 90 || $p1x < -180 || $p1x > 180 || $p2x < -180 || $p2x > 180)
+{
+	echo("Out of bounds");
+}
+
+?>
 
 </body>
 </html>
