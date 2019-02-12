595fa7dc6da935883ea5b6193a13f641b54b0e67
ali_hassam <alykhanhssm@gmail.com>
2019-02-12 15:30:19 -0700

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,9 +3,11 @@
 <html>
 <body>
 
-<?php
-echo "My first PHP script!";
-?> 
+Point 1 Latitude: <?php echo $_GET["p1x"]; ?><br>
+Point 1 Longitude: <?php echo $_GET["p1y"]; ?> <br>
+Point 2 Latitude: <?php echo $_GET["p2x"]; ?><br>
+Point 2 Longitude: <?php echo $_GET["p2y"]; ?>
+
 
 </body>
 </html>
