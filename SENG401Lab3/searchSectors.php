37ced7f176d4c83c915696ced0adaedfd4d20817
sifanxu1996 <sifan.xua@gmail.com>
2019-02-24 22:56:41 -0700

    lab3task1
    task 1.1 + 1.2 complete

---
+++ SENG401Lab3/searchSectors.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<?php
+    $host='localhost';
+    $db = 'SENG401'; //use pgadmin to create a database e.g. SENG401
+    $username = 'postgres'; //usually postgres
+    $password = 'S3cr3T5'; //usually postgres
+    $port = 5432;
+    $dsn = "pgsql:host=$host; port=$port; dbname=$db; user=$username; password=$password";
+    
+    $mode = intval($_GET['mode']);
+    // 1 is JSON
+    // 2 is XML
+    // 3 is Comma
+    // 4 is Table
+
+    $sector = intval($GET['sector']);
+    // 1 is SW
+    // 2 is NW
+    // 3 is SE
+    // 4 is NE
+
+    $keyword = $_GET['keyword'];
+
+    try{
+        // create a PostgreSQL database connection
+        $conn = new PDO($dsn);
+
+        // display a message if connected to the PostgreSQL successfully
+        if($conn){
+            echo "Connected to the <strong>$db</strong> database successfully! <br>";
+
+            $queryStatement = 'SELECT * FROM CalgarySchools;';..
+
+            if ($keyword != "" && !is_null($keyword)) {
+                $queryStatement = "SELECT * FROM CalgarySchools WHERE name LIKE '%' || '". $keyword ."' || '%';";
+            }
+
+            echo "Query: $queryStatement <br>";
+
+            $query = $conn->query($queryStatement);
+            $results = $query->fetchAll();
+
+            // JSON mode
+            if ($mode == 1) {
+                $json_string = json_encode($results, JSON_PRETTY_PRINT);
+                echo ("<pre>".$json_string."</pre>");
+            }
+
+            // XML mode
+            if ($mode == 2) {
+                $xml_calgary_schools = new SimpleXMLElement("<?xml version=\"1.0\"?><calgary_schools></calgary_schools>");
+                array_to_xml($results, $xml_calgary_schools);
+                
+                $dom = new DOMDocument('1.0');
+                $dom = dom_import_simplexml($xml_calgary_schools)->ownerDocument;
+                $dom->preserveWhiteSpace = false;
+                $dom->formatOutput = true;
+                $formattedXML = $dom->saveXML();
+
+                //saving generated xml file
+                $dom->save("calgarySchools.xml");
+
+                echo("<pre>".htmlspecialchars($formattedXML)."</pre>");
+            }
+
+            // Comma mode
+            if ($mode == 3) {
+                foreach ($results as $result) {
+                    echo $result['name'] . ", " . $result['type'] . ", " . $result['sector'] . ", " . $result['address'] . ", " . $result['city'] . ", " . $result['province'] . "," . $result['longitude'] . ", " . $result['latitude'] . ", " . $result['postalcode'] . "<br>";
+                }
+            }
+
+            // Table mode
+            if ($mode == 4) {
+                echo "<table>
+                <tr>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Sector</th>
+                <th>Address</th>
+                <th>City</th>
+                <th>Province</th>
+                <th>Longitude</th>
+                <th>Latitude</th>
+                <th>Postal Code</th>
+                </tr>";
+                foreach($results as $result) {
+                    echo "<tr>";
+                    echo "<td>" . $result['name'] . "</td>";
+                    echo "<td>" . $result['type'] . "</td>";
+                    echo "<td>" . $result['sector'] . "</td>";
+                    echo "<td>" . $result['address'] . "</td>";
+                    echo "<td>" . $result['city'] . "</td>";
+                    echo "<td>" . $result['province'] . "</td>";
+                    echo "<td>" . $result['longitude'] . "</td>";
+                    echo "<td>" . $result['latitude'] . "</td>";
+                    echo "<td>" . $result['postalcode'] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+        }
+
+    } catch (PDOException $e){
+        // report error message
+        echo $e->getMessage();
+    }
+
+    //function definition to convert array to xml
+    function array_to_xml($array, &$simpleXMLElement) {
+        foreach($array as $key => $value) {
+            if(is_array($value)) {
+                if(!is_numeric($key)){
+                    $subnode = $simpleXMLElement->addChild("$key");
+                    array_to_xml($value, $subnode);
+                }else{
+                    $subnode = $simpleXMLElement->addChild("item$key");
+                    array_to_xml($value, $subnode);
+                }
+            }else {
+                $simpleXMLElement->addChild("$key",htmlspecialchars("$value"));
+            }
+        }
+    }
+?>
+
+</body>
+</html>
+
+
