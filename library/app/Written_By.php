896f97073e03e8d6e8290261b0f274de13c7997c
ali_hassam <alykhanhssm@gmail.com>
2019-03-19 16:37:16 -0600

    migrations

---
+++ library/app/Written_By.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Written_By extends Model
+{
+ 	public function book()
+ 	{
+ 		return $this->hasMany(Book::class)
+ 	}
+ 	
+ 	public function author()
+ 	{
+ 		return $this->hasMany(Author::class)
+ 	}
+}
---
+++ library/database/migrations/2019_03_19_223059_create_written_by_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWrittenByTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('written_by', function (Blueprint $table) {
+            $table->integer('author_id');
+            $table->integer('book_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('written_by');
+    }
+}
