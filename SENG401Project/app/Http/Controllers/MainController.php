0adabedc383246356b0413110ad2f4d10c2529ce
ali_hassam <alykhanhssm@gmail.com>
2019-04-06 23:01:07 -0600

    got something working

---
+++ SENG401Project/app/Http/Controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Alaouy\Youtube\Facades\Youtube;
+
+
+
+class MainController extends Controller
+{
+
+
+
+    public function index()
+    {
+        $video = Youtube::getVideoInfo('rie-hPVJ7Sw');
+
+        return view("main", ["video"=>$video]);
+    }
+}
---
+++ SENG401Project/resources/views/main.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Main</title>
+    </head>
+    <body>
+            <div class="content">
+                <div class="title m-b-md">
+                    Main
+                </div>
+
+                <div class="video">
+
+                    {{json_encode($video, true)}}
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
