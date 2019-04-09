40a9572212ae801d96fe2b2da9d242795599f1c1
ali_hassam <alykhanhssm@gmail.com>
2019-04-09 17:21:49 -0600

    page

--- SENG401Project/resources/views/users/show.blade.php
+++ SENG401Project/resources/views/users/show.blade.php
@@ -1,1 +1,43 @@
-{{ $user->name }}
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Welcome to {{$user -> playlist_title}} <br>
+                    This is the page of {{ $user->name }}! <br>
+                    <img src= {{$user->picture_url}}>
+
+
+                    <a href = "/users/{{ $user->id }}/edit">
+                        <button>Edit Profile</button>
+                    </a>
+                    
+                    <br>
+                    <form method = 'POST' action = "/users/{{ $user->id }}">
+
+                    Please Enter a Video URL to Add to Playlist (Yo Clean this Up):<br>
+                        <input type="text" name="firstname" value="Mickey">
+                    <br>
+
+
+                    </form>
+
+
+                    <div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
